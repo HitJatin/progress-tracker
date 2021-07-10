9dddbc825e904c3a99479b3b6774fdad44cda7d5
HitJatin <adriano23012001@gmail.com>
2021-07-10 06:42:18 +0530

    Updates

--- precache-manifest.f6919491ddff97686100899773f5fee9.js
+++ precache-manifest.f6919491ddff97686100899773f5fee9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "589b7b706530953c2f88bfe8263a8052",
+    "revision": "0cd01fa4b15707cf4e3fb81ac0d2e127",
     "url": "/progress-tracker/index.html"
   },
   {
-    "revision": "5bd9d5de1d7ff171e9bc",
+    "revision": "a9dc0e1db0c2071a6edf",
     "url": "/progress-tracker/static/css/2.276bfa6f.chunk.css"
   },
   {
-    "revision": "e276da0ab127173ab47a",
+    "revision": "51d58bc9b9b9a273966b",
     "url": "/progress-tracker/static/css/main.b8122b06.chunk.css"
   },
   {
-    "revision": "5bd9d5de1d7ff171e9bc",
-    "url": "/progress-tracker/static/js/2.f8a15f04.chunk.js"
+    "revision": "a9dc0e1db0c2071a6edf",
+    "url": "/progress-tracker/static/js/2.d60b4db5.chunk.js"
   },
   {
     "revision": "d2966845b94a3318bf32eecc7af8015d",
-    "url": "/progress-tracker/static/js/2.f8a15f04.chunk.js.LICENSE.txt"
+    "url": "/progress-tracker/static/js/2.d60b4db5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e276da0ab127173ab47a",
-    "url": "/progress-tracker/static/js/main.4d339cb9.chunk.js"
+    "revision": "51d58bc9b9b9a273966b",
+    "url": "/progress-tracker/static/js/main.621dda19.chunk.js"
   },
   {
     "revision": "2ab18c1ba769843b63d0",
